28101e926dfe9acc644558f319f6d25c48940ea0
r-ume <baymax.blackandwhite@gmail.com>
2017-10-09 21:26:00 +0900

    added message model

---
+++ app/models/message.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id           :integer          not null, primary key
+#  sender_id    :integer          not null
+#  recipient_id :integer          not null
+#  body         :string(255)      default("NO BOBY"), not null
+#  image        :string(255)      default("NO IMAGE"), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
+class Message < ApplicationRecord
+
+  # Association
+  belongs_to :sender,    class_name: 'User', foreign_key: 'sender_id'
+  belongs_to :recipient, class_name: 'User', foreign_key: 'recipient_id'
+
+  # Validation
+  validates :sender_id, :recipient_id, numericality: true, presence: true
+  validates :has_body_or_image,        presence: true
+
+  def has_body_or_image
+    body.presence || image.presence
+  end
+end
