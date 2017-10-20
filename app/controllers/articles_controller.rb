d216b80067e12c586fa140048e6aea4e7687321a
r-ume <baymax.blackandwhite@gmail.com>
2017-10-20 11:34:42 +0900

    defined articles#index action

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,43 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
+
+  def index
+    @articles = paginate ArticleDecorator.decorate_collection(Article.all.includes(:user)), per_page: Article::PER_PAGE
+
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @articles, each_serializer: ArticleSerializer }
+    end
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def set_article
+    article_id = params[:id].to_i
+
+    # レコードが見つからなかったら、404を吐き出すようにする
+    # if article_id <= 0
+    # end
+
+    @article = Article.find(article_id).decorate
+  end
+end
