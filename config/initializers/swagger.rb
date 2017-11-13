3736a7edf1df55507f6334c56b030b4949bd12e9
r-ume <baymax.blackandwhite@gmail.com>
2017-11-13 14:29:36 +0900

    added option url in swagger initializer

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,3 +1,4 @@
+GrapeSwaggerRails.options.url = '/api/v1/swagger_doc'
 GrapeSwaggerRails.options.before_action do |request|
   GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
 end
