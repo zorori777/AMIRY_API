30894e7f198dd1900478ca133138ce07574eba2f
r-ume <baymax.blackandwhite@gmail.com>
2017-11-30 16:43:25 +0900

    added basic authentication to swagger ui

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,5 +1,8 @@
 GrapeSwaggerRails.options.url = '/api/v1/swagger_doc'
 GrapeSwaggerRails.options.before_action do |request|
   GrapeSwaggerRails.options.app_url = request.protocol + request.host_with_port
+  authenticate_or_request_with_http_basic do |username, pass|
+    username == ENV['BASIC_AUTH_USERNAME'] && pass == ENV['BASIC_AUTH_PASSWORD']
+  end
 end
 GrapeSwaggerRails.options.app_name = 'AMIRY'
