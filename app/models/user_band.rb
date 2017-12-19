8b92e741392dcd498aab13e445bb60088eca74f7
r-ume <baymax.blackandwhite@gmail.com>
2017-12-19 18:22:40 +0900

    added UserBand model

---
+++ app/models/user_band.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: user_bands
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  band_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class UserBand < ApplicationRecord
+
+  # Validation
+  validates :user_id, :band_id, presence: true, numericality: { only_integer: true }
+
+  # Association
+  belongs_to :user
+  belongs_to :band
+
+end
