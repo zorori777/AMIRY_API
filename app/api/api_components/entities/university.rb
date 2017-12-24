04c05ca908d398829cd8ad2e715117b4bd74b980
r-ume <baymax.blackandwhite@gmail.com>
2017-12-24 13:38:55 +0900

    added is_array option

--- app/api/api_components/entities/university.rb
+++ app/api/api_components/entities/university.rb
@@ -4,7 +4,7 @@ module APIComponents
       expose :id,                  documentation: { required: true, type: 'Integer', desc: 'The primary id.' }
       expose :name,                documentation: { required: true, type: 'String',  desc: 'The name of the university.' }
       expose :circles_num,         documentation: { required: true, type: 'Integer', desc: 'The number of circles that the university has.' }
-      expose :circles_id_and_name, documentation: { required: true, type: 'Array',   desc: 'The ids and names of circles that the university has.' }
+      expose :circles_id_and_name, documentation: { required: true, type: 'Hash',    desc: 'The ids and names of circles that the university has.', is_array: true }
     end
   end
 end
