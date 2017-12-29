591bbd5b1e69be2c026ae1464100ab5ab1d2a5fd
r-ume <baymax.blackandwhite@gmail.com>
2017-12-29 21:07:37 +0900

    added facebook_friend model

---
+++ app/models/facebook_friend.rb
@@ -0,0 +1,29 @@
+# == Schema Information
+#
+# Table name: facebook_friends
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer          unsigned, not null
+#  facebook_id :integer          default(0), unsigned, not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class FacebookFriend < ApplicationRecord
+
+  # Association
+  belongs_to :user
+
+  # Validation
+  validates :user_id,                         uniqueness: { scope: :facebook_id }
+  validates :user_id, :facebook_id,           numericality: { only_integer: true }, presence: true
+  validate  :not_same_user_id_and_facebook_id
+
+  # Custom Validation
+  def not_same_user_id_and_facebook_id
+    return unless self.facebook_id == User.find_by(id: self.user_id).facebook_id
+    errors.add('facebook_id', 'the user itself was about to get inserted.')
+    errors.add('user_id', 'the user itself was about to get inserted.')
+  end
+
+end
