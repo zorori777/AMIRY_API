8c1cd7d356787a4cb73ac09a89c605d8157103f7
r-ume <baymax.blackandwhite@gmail.com>
2017-12-31 21:32:38 +0900

    added lives#show action

--- app/api/api_components/controllers/lives_controller.rb
+++ app/api/api_components/controllers/lives_controller.rb
@@ -1,6 +1,7 @@
 module APIComponents
   module Controllers
     class LivesController < ApiController
+      # index
       desc 'Return all lives' do
         http_codes([
           { code: 200, message: 'live', model: Entities::Live }
@@ -11,6 +12,23 @@ module APIComponents
       end
       get '/' do
         present Live.includes(:user).newest.passed.page(params[:page]), with: Entities::Live
+      end
+
+      # show
+      desc 'Return a Live Object' do
+        http_codes([
+          { codes: 200, message: 'live', model: Entities::Live }
+        ])
+      end
+      params do
+        requires :id, type: Integer, desc: 'the id of the live.'
+      end
+      get '/:id' do
+        live = Live.find_by(id: params[:id])
+        unless live.present?
+          Errors::RecordNotFoundError.new(id: params[:id], model: 'live')
+        end
+        present Live.find(params[:id]), with: Entities::Live
       end
     end
   end
--- app/api/api_components/entities/live.rb
+++ app/api/api_components/entities/live.rb
@@ -2,14 +2,14 @@ module APIComponents
   module Entities
     # Exposed properties in Live model
     class Live < Grape::Entity
-      expose :id,                 documentation: { required: true, type: 'Integer', desc: 'The primary id of the circle.' }
-      expose :circle_name,        documentation: { required: true, type: 'String',  desc: 'The name of the circle where a live is to be held.' }
-      expose :description,        documentation: { required: true, type: 'String',  desc: 'The description of the live.' }
-      expose :max_capacity,       documentation: { required: true, type: 'Integer', desc: 'The capacity of the live.' }
-      expose :reservations_count, documentation: { required: true, type: 'Integer', desc: 'The reservations count for the live.' }
-      expose :type,               documentation: { required: true, type: 'Integer', desc: 'The type of the live.' }
-      expose :hold_at,            documentation: { required: true, type: 'String',  desc: 'When the live is to be held at.' }
-      expose :available_seats,    documentation: { required: true, type: 'Integer', desc: 'The number of available seats.' }
+      expose :id,                  documentation: { required: true, type: 'Integer', desc: 'The primary id of the circle.' }
+      expose :circle_name,         documentation: { required: true, type: 'String',  desc: 'The name of the circle where a live is to be held.' }
+      expose :description,         documentation: { required: true, type: 'String',  desc: 'The description of the live.' }
+      expose :max_capacity,        documentation: { required: true, type: 'Integer', desc: 'The capacity of the live.' }
+      expose :reservations_count,  documentation: { required: true, type: 'Integer', desc: 'The reservations count for the live.' }
+      expose :type,                documentation: { required: true, type: 'Integer', desc: 'The type of the live.' }
+      expose :hold_at,             documentation: { required: true, type: 'String',  desc: 'When the live is to be held at.' }
+      expose :available_seats_num, documentation: { required: true, type: 'Integer', desc: 'The number of available seats.' }
     end
   end
 end
