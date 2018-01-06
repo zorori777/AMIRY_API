b37adad498536f4da611717d1334b2ba350ade4b
r-ume <baymax.blackandwhite@gmail.com>
2018-01-06 19:27:48 +0900

    replaced matching with like in likes_controller.rb

--- app/api/api_components/controllers/likes_controller.rb
+++ app/api/api_components/controllers/likes_controller.rb
@@ -1,50 +1,67 @@
 module APIComponents
   module Controllers
-    class MatchingsController < ApiController
+    class LikesController < ApiController
       # create
-      desc 'Create a matching.' do
+      desc 'Create a like.' do
         detail <<~DETAIL
-          Create a new matching object
+          Create a new like object
         DETAIL
         http_codes([
-          { code: 200, message: 'Matching', model: Entities::Matching },
-          { code: 400, message: 'Error',    model: Entities::Error }
+          { code: 200, message: 'Like',  model: Entities::Like },
+          { code: 400, message: 'Error', model: Entities::Error }
         ])
       end
       params do
-        requires :like_id,      type: Integer, desc: 'The id of the like that the recipient approved.'
-        requires :recipient_id, type: Integer, desc: 'The id of the user who receives the matching.'
-        requires :sender_id,    type: Integer, desc: 'The id of the user who sends the introduction.'
+        requires :sender_id,    type: Integer, desc: 'The id of the user who sends the like.'
+        requires :recipient_id, type: Integer, desc: 'The id of the user who receives the like.'
       end
       post '/' do
-        matching = Matching.new(like_id: params[:like_id], recipient_id: params[:recipient_id], sender_id: params[:sender_id])
-        matching.save!
-        present matching, with: Entities::Matching
+        like = Like.new(recipient_id: params[:recipient_id], sender_id: params[:sender_id])
+        begin
+          like.save!
+          Inserters::Like::Request.perform_async(like.id)
+          Notifiers::Like::Request.perform_async(like.id)
+          present like, with: Entities::like
+        rescue => error
+          Errors::InternalServerError.new(message: error)
+        end
       end
 
       # Update
-      desc 'Update the given matching.' do
+      desc 'Update the given like.' do
         detail <<~DETAIL
-          Update the given matching object.
+          Update the given like object.
         DETAIL
         http_codes([
-          { code: 200, message: 'Matching', model: Entities::Matching },
-          { code: 400, message: 'Error',    model: Entities::Error }
+          { code: 200, message: 'Like',  model: Entities::Like },
+          { code: 400, message: 'Error', model: Entities::Error }
         ])
       end
       params do
         requires :id,         type: Integer, desc: 'The id of the matching.'
-        requires :acceptance, type: Integer, desc: 'The acceptance of the matching.'
+        requires :acceptance, type: Integer, desc: 'The acceptance of the like.'
       end
-      post '/' do
-        matching = Matching.find_by(id: params[:id])
-        unless matching.present?
-          Errors::RecordNotFoundError.new(id: params[:id], model: 'Matching')
+      put '/' do
+        like = Like.find_by(id: params[:id])
+        new_acceptance = params[:acceptance]
+        unless like.present?
+          Errors::RecordNotFoundError.new(id: params[:id], model: 'Like')
         end
-        matching.update_acceptance!(acceptance: params[:acceptance])
-        Notifiers::Matching.perform_async(matching.sender_id, matching.recipient_id)
-        Inserters::Matching.perform_async(matching.sender_id, matching.recipient_id, matching.id)
-        present matching, with: Entities::Matching
+
+        like.acceptance = new_acceptance
+
+        if like.accepted?
+          ActiveRecord::Base.transaction do
+            like.update_acceptance!(acceptance: new_acceptance)
+            matching = Matching.new(like_id: like.id, sender_id: like.sender_id, recipient_id: like.recipient_id)
+            matching.save!
+          end
+
+          Notifiers::Like::Approval.perform_async(like.id)
+          Inserters::Like::Approval.perform_async(like.id)
+
+          present like, with: Entities::Like
+        end
       end
     end
   end
