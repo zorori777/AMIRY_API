9fa5f60bdf20b2ff362bca63dedf1c066a229798
r-ume <baymax.blackandwhite@gmail.com>
2018-01-11 10:42:01 +0900

    added message entities

---
+++ app/api/api_components/entities/message.rb
@@ -0,0 +1,11 @@
+module APIComponents
+  module Entities
+    class Message < Grape::Entity
+      expose :sender_name,    documentation: { required: true, type: String, desc: 'The display name of the sender.' }
+      expose :recipient_name, documentation: { required: true, type: String, desc: 'The display name of the recipient.' }
+      expose :body,           documentation: { required: true, type: String, desc: 'The body of the message.' }
+      expose :image,          documentation: { required: true, type: String, desc: 'The image of the messsage.' }
+      expose :created_at,     documentation: { required: true, type: String, desc: 'When the message got sent.' }
+    end
+  end
+end
