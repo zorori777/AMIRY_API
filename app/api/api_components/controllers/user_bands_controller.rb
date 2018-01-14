ec447c839392e83009b3f79c93068296b335971f
r-ume <baymax.blackandwhite@gmail.com>
2018-01-14 13:49:47 +0900

    added user_bands#create action

---
+++ app/api/api_components/controllers/user_bands_controller.rb
@@ -0,0 +1,33 @@
+module APIComponents
+  module Controllers
+    class UserBandsController < ApiController
+      # create
+      desc 'Create UserBand Objects' do
+        http_codes([
+          { code: 200, message: 'UserBand', model: Entities::UserBand }
+        ])
+      end
+      params do
+        requires :user_ids, type: Array[Integer], desc: 'The ids of the user.'
+        requires :band_id, type: Integer,        desc: 'The id of the band.'
+      end
+      post '/' do
+        band = Band.find_by(id: params[:band_id])
+        unless band.present?
+          Errors::RecordNotFoundError.new(id: params[:band_id], model: 'Band')
+        end
+
+        if band.additional_people_num_within_range?(additional_people_num: params[:user_ids].size)
+          Errors::BadRequestError.new(message: 'Band\'s people num is out of the range.')
+        end
+
+        user_bands = params[:user_ids].map do |user_id|
+          user_band = UserBand.new(band_id: params[:band_id], user_id: user_id)
+          user_band.save!
+        end
+
+        present user_bands, with: Entity::UserBand
+      end
+    end
+  end
+end
