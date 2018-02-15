0ecdeb1616f06f2a9bb3d3e1558d177fe8181871
r-ume <baymax.blackandwhite@gmail.com>
2018-02-15 17:25:04 +0900

    added response_helper.rb

---
+++ app/api/api_components/helpers/response_helper.rb
@@ -0,0 +1,44 @@
+require 'date'
+
+module APIComponents
+  module Helpers
+    module ResponseHelper
+
+      def _get_server_meta_data
+        {
+          server_clock: Time.zone.now
+        }
+      end
+
+      ## 200 OK
+      def present_ok(data, data_entity=nil, **opts)
+        status 200
+        present_success(data, data_entity, opts)
+      end
+
+      def present_success(data, data_entity, **opts)
+        options = data_entity ? { with: data_entity } : {}
+        options.merge!(opts)
+        present data, options
+      end
+
+      def error_404!(message = 'Not Found')
+        _error(message, 404)
+      end
+
+      def _error(message, code, *args)
+        if args[0].is_a? Exception
+          message ||= args[0].message
+        end
+
+        error!({
+          error: {
+            code: 404,
+            message: message,
+          },
+          with: APIComponents::Entities::Error
+        }, code)
+      end
+    end
+  end
+end
